f43504b4f18ef2d8f831329d34788dc8b2df6a5d
Viegas <mvrdgs@gmail.com>
2021-07-09 23:49:59 -0300

    Updates

---
+++ precache-manifest.23965064f73202ad8c3f1b1595a67df4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37eafc0e728d56c5a747683078c817d4",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "0bcce9798a8b8db68304",
+    "url": "/trivia/static/css/main.2381d66a.chunk.css"
+  },
+  {
+    "revision": "4c4f93d9d67782bcf24e",
+    "url": "/trivia/static/js/2.9a94202f.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia/static/js/2.9a94202f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bcce9798a8b8db68304",
+    "url": "/trivia/static/js/main.ce24587a.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
